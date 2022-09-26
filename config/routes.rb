675b790b5ca702bbb23bf29022fdc37af9123163
Ubuntu <ubuntu@ip-172-31-44-116.ap-northeast-1.compute.internal>
2022-09-26 08:50:20 +0000

    finish 2-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :todos
-  resources :goals
+   resources :goals do
+   resources :todos do 
+    member do
+       get "sort"
+     end
+   end 
+ end
   devise_for :users
  
 
