7e86909e75e1e470fd1b226c7a8008b6633dd6db
Ubuntu <ubuntu@ip-172-31-44-116.ap-northeast-1.compute.internal>
2022-09-27 07:49:09 +0000

    finish 2-8

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,7 @@
 class Goal < ApplicationRecord
   belongs_to :user
+ has_many :todos, -> { order("done ASC, position ASC").includes(:goal) }, dependent: :destroy
+ 
+  validates :title, presence: true
+
 end
